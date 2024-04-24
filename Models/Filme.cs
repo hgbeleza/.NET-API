208980889aadeb730cd45c174d8b796abcb639fe
hgbeleza <developer.beleza06@gmail.com>
2024-04-24 16:19:44 -0300

    first commit

---
+++ Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Titulo is required")] 
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Genero is required")]
+        [MaxLength(50)]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "Duracao is required")]
+        [Range(70, 600)]
+        public int Duracao { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# FILMES REST API .NET
+
+Estrutura basica de uma API REST em .NET8
